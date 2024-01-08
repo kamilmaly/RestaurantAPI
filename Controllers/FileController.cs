eb2c70c812d176f0da3ef85d25c103edb56e53a7
kamilmaly <k.malysa94@gmail.com>
2024-01-08 22:49:33 +0100

    Returning static files

---
+++ Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("file")]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        public ActionResult GetFile([FromQuery] string fileName)
+        {
+            var rootPath = Directory.GetCurrentDirectory();
+
+            var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+            var fileExists = System.IO.File.Exists(filePath);
+
+            if (!fileExists)
+            {
+                return NotFound();
+            }
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+
+            contentProvider.TryGetContentType(fileName, out string contentType);
+
+            var fileContents = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileContents, contentType, filePath);
+        }
+    }
+}
