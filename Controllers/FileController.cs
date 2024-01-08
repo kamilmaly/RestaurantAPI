1240ea66040c885ce55bed1dfde816cdab960a30
kamilmaly <k.malysa94@gmail.com>
2024-01-08 23:17:31 +0100

    Uploading files to the server

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using System.IO;
@@ -29,6 +30,27 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, filePath);
+        }
+        [HttpPost]
+        public ActionResult Upload([FromForm] IFormFile file)
+        {
+            if(file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+
+                var fileName = file.FileName;
+
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
